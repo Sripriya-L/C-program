1996d1546101c912b2671b1314cac1c910c9fa4c
Sripriya-L <srilekshu2006@gmail.com>
2026-02-11 10:22:27 +0530

    Add files via upload

---
+++ Topic 2 program 1.c
@@ -0,0 +1,8 @@
+// 1.Declare an integer variable
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+    printf("%d",a);
+    return 0;
+}
---
+++ Topic 3 program 1.c
@@ -0,0 +1,13 @@
+// 1.Check Equality
+#include<stdio.h>
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if (a==b) {
+        printf("Equal");
+    }
+    else {
+        printf("Not Equal");
+    }
+    return 0;
+}
