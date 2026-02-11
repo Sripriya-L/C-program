aff95506544557d95f9fac8bef014eb92f4a9f8d
Sripriya-L <srilekshu2006@gmail.com>
2026-02-11 10:20:01 +0530

    Add files via upload

---
+++ program 50.c
@@ -0,0 +1,13 @@
+// 50.Logical AND check
+#include<stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if (n>=10 && n<=50) {
+        printf("YES\n");
+    }
+    else {
+        printf("NO\n");
+    }
+    return 0;
+}
